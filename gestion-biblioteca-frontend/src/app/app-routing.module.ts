6fa61cc04db54f8f1dd114a6b759b3d7fa8aec44
CaroZTSDWeb <carozamudia@gmail.com>
2022-09-21 13:14:43 -0300

    Frontend: componente: Inicio

--- gestion-biblioteca-frontend/src/app/app-routing.module.ts
+++ gestion-biblioteca-frontend/src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { LibrosComponent } from './pages/libros/libros.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 const routes: Routes = [
-  { path: '', component: NotFoundComponent },
+  { path: '', component: InicioComponent },
+  { path: 'notfound', component: NotFoundComponent},
   { path: 'libros', component: LibrosComponent },
   { path: 'prestamos', component: NotFoundComponent },
   { path: 'perfil', component: NotFoundComponent },
   { path: 'perfil/opciones', component: NotFoundComponent },
   { path: 'condiciones-de-uso', component: NotFoundComponent },
+  { path: '**', component: InicioComponent}
 ];
 
 @NgModule({
--- gestion-biblioteca-frontend/src/app/pages/pages.module.ts
+++ gestion-biblioteca-frontend/src/app/pages/pages.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LibrosComponent } from './libros/libros.component';
 import { SearchWidgetComponent } from './libros/search-widget/search-widget.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
-  declarations: [NotFoundComponent, LibrosComponent, SearchWidgetComponent],
+  declarations: [NotFoundComponent, LibrosComponent, SearchWidgetComponent, InicioComponent],
   imports: [CommonModule, SharedModule],
-  exports: [NotFoundComponent, LibrosComponent]
+  exports: [NotFoundComponent, LibrosComponent, InicioComponent]
 })
 export class PagesModule {}
