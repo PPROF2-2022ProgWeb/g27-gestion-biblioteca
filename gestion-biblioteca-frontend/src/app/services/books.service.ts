52d89bddf03d0e094927dbdea6a94fe82669eb67
Selien Yorbandi <selienyorbandi@gmail.com>
2022-09-21 16:14:51 -0300

    Frontend: Services properties

--- gestion-biblioteca-frontend/src/app/services/books.service.ts
+++ gestion-biblioteca-frontend/src/app/services/books.service.ts
@@ -1,3 +1,4 @@
+import { API_URL } from './services.properties';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -6,27 +7,27 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class BooksService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   public getBooks(): Observable<any> {
-    return this.http.get<any>('http://localhost:8082/books');
+    return this.http.get<any>(`${API_URL}books`);
   }
 
   public getBookById(id: number): Observable<any> {
-    return this.http.get<any>(`http://localhost:8082/books/id/${id}`);
+    return this.http.get<any>(`${API_URL}books/id/${id}`);
   }
 
   public getBooksByTitle(title: string): Observable<any> {
-    return this.http.get<any>(`http://localhost:8082/books/title/${title}`);
+    return this.http.get<any>(`${API_URL}books/title/${title}`);
   }
 
   public getBooksByAuthor(author: string): Observable<any> {
-    return this.http.get<any>(`http://localhost:8082/books/author/${author}`);
+    return this.http.get<any>(`${API_URL}books/author/${author}`);
   }
 
   public getBooksByCategory(category: string): Observable<any> {
     return this.http.get<any>(
-      `http://localhost:8082/books/category/${category}`
+      `${API_URL}books/category/${category}`
     );
   }
 
