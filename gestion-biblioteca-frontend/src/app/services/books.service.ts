cbb574492b1f0499d371c6ad475d893545d796f7
Selien Yorbandi <selienyorbandi@gmail.com>
2022-09-21 22:42:23 -0300

    Frontend: función de búsqueda de libros por titulo, isbn, autor y categoria

--- gestion-biblioteca-frontend/src/app/services/books.service.ts
+++ gestion-biblioteca-frontend/src/app/services/books.service.ts
@@ -9,26 +9,19 @@ import { Injectable } from '@angular/core';
 export class BooksService {
   constructor(private http: HttpClient) { }
 
-  public getBooks(): Observable<any> {
-    return this.http.get<any>(`${API_URL}books`);
-  }
-
-  public getBookById(id: number): Observable<any> {
-    return this.http.get<any>(`${API_URL}books/id/${id}`);
-  }
-
-  public getBooksByTitle(title: string): Observable<any> {
-    return this.http.get<any>(`${API_URL}books/title/${title}`);
-  }
-
-  public getBooksByAuthor(author: string): Observable<any> {
-    return this.http.get<any>(`${API_URL}books/author/${author}`);
-  }
-
-  public getBooksByCategory(category: string): Observable<any> {
-    return this.http.get<any>(
-      `${API_URL}books/category/${category}`
-    );
+  public getBooksBy({ type, arg }: { type?: string, arg?: string }): Observable<any> {
+    switch (type) {
+      case "title":
+        return this.http.get<any>(`${API_URL}books/title/${arg}`);
+      case "isbn":
+        return this.http.get<any>(`${API_URL}books/id/${arg}`);
+      case "author":
+        return this.http.get<any>(`${API_URL}books/author/${arg}`);
+      case "category":
+        return this.http.get<any>(`${API_URL}books/category/${arg}`);
+      default:
+        return this.http.get<any>(`${API_URL}books`);
+    }
   }
 
   /* private createBook(book: IBook): Observable<any> {
